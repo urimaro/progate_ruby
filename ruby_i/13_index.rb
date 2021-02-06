47b1119fcea55408473f17b0ecb580822974e6e0
Goh Matsumoto <urimaro@gmail.com>
2021-02-07 00:10:30 +0900

    13. Booleans & Comparison Operators (1) - 3. Booleans and Conditions

---
+++ ruby_i/13_index.rb
@@ -0,0 +1,12 @@
+score = 80
+
+# 「score > 80」を出力してください
+puts score > 80
+
+# 「score <= 80」を出力してください
+puts score <= 80
+
+# scoreの値が80以下の場合に、「がんばりましょう」と出力してください
+if score <= 80
+  puts "がんばりましょう"
+end
