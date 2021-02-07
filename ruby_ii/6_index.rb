b5dae6119c56cf63cd3b999f6bf6db324fd2dc04
Goh Matsumoto <urimaro@gmail.com>
2021-02-07 16:20:25 +0900

    6. Using Hashes - 2. Hashes and Symbols

---
+++ ruby_ii/6_index.rb
@@ -0,0 +1,16 @@
+exam = {"subject" => "Math", "score" => 80}
+
+# キー「"subject"」の値を出力してください
+puts exam["subject"]
+
+# キー「"subject"」の値を「"Science"」に更新してください
+exam["subject"] = "Science"
+
+# もう一度、キー「"subject"」の値を出力してください
+puts exam["subject"]
+
+# キーが「"grade"」、値が「"good"」の要素を追加してください
+exam["grade"] = "good"
+
+# キー「"grade"」の値を出力してください
+puts exam["grade"]
