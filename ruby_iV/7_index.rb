fdd9937c32979c980253d78ab9201a8a55b2e639
Goh Matsumoto <urimaro@gmail.com>
2021-02-11 01:05:00 +0900

    10. Classes and Instances Review - 2. Instance Methods

--- ruby_iV/7_index.rb
+++ ruby_iV/7_index.rb
@@ -5,10 +5,20 @@ class Menu
   def info
     return "#{self.name} #{self.price}円"
   end
+
+  def get_total_price(count)
+    total_price = self.price * count
+
+    if count >= 3
+      total_price -= 100
+    end
+
+    return total_price
+  end
 end
 
 menu1 = Menu.new
 menu1.name = "ピザ"
 menu1.price = 800
 
-puts menu1.info
+puts menu1.get_total_price(3)
