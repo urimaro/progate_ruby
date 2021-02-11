fc15ac5cc0e3acf2d56ae682db33621a025784e7
Goh Matsumoto <urimaro@gmail.com>
2021-02-11 15:06:17 +0900

    5. Adding Instance Methods - 1. Class Inheritance

--- ruby_v/food.rb
+++ ruby_v/food.rb
@@ -2,4 +2,8 @@ require "./menu"
 
 class Food < Menu
   attr_accessor :calorie
+
+  def calorie_info
+    return "#{self.name}は#{self.calorie}kcalです"
+  end
 end
--- ruby_v/index.rb
+++ ruby_v/index.rb
@@ -2,10 +2,6 @@ require "./food"
 require "./drink"
 
 food1 = Food.new(name: "ピザ", price: 800)
-drink1 = Drink.new(name: "コーラ", price: 300)
-
 food1.calorie = 700
-puts food1.calorie
 
-drink1.amount = 500
-puts drink1.amount
+puts food1.calorie_info
