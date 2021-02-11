43215420d834d11671922942476903fee20210c7
Goh Matsumoto <urimaro@gmail.com>
2021-02-11 15:17:50 +0900

    7. Override (1) - 1. Class Inheritance

--- ruby_v/drink.rb
+++ ruby_v/drink.rb
@@ -2,4 +2,8 @@ require "./menu"
 
 class Drink < Menu
   attr_accessor :amount
+
+  def info
+    return "#{self.name} #{self.price}円 (#{self.amount}mL)"
+  end
 end
--- ruby_v/index.rb
+++ ruby_v/index.rb
@@ -2,9 +2,13 @@ require "./food"
 require "./drink"
 
 food1 = Food.new(name: "ピザ", price: 800)
+food1.calorie = 700
 food2 = Food.new(name: "すし", price: 1000)
+food2.calorie = 600
 drink1 = Drink.new(name: "コーラ", price: 300)
+drink1.amount = 500
 drink2 = Drink.new(name: "お茶", price: 200)
+drink2.amount = 400
 
 menus = [food1, food2, drink1, drink2]
 
