ba8b6d8665a97ea433f1cb848cfba55214a1b440
Goh Matsumoto <urimaro@gmail.com>
2021-02-11 23:49:16 +0900

    8. Override (2) - 1. Class Inheritance

--- ruby_v/food.rb
+++ ruby_v/food.rb
@@ -3,6 +3,12 @@ require "./menu"
 class Food < Menu
   attr_accessor :calorie
 
+  def initialize(name:, price:, calorie:)
+    self.name = name
+    self.price = price
+    self.calorie = calorie
+  end
+
   def info
     return "#{self.name} #{self.price}円 (#{self.calorie}kcal)"
   end
