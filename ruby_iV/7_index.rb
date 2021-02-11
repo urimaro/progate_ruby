ff2a8129aade564ee2d1a265e6c756220e461d6a
Goh Matsumoto <urimaro@gmail.com>
2021-02-11 12:34:04 +0900

    14. Displaying the Menu (1) - 3. The Food Ordering App

--- ruby_iV/7_index.rb
+++ ruby_iV/7_index.rb
@@ -1,5 +1,12 @@
 require "./menu"
 
-menu1 = Menu.new(name: "すし", price: 1000)
+menu1 = Menu.new(name: "ピザ", price: 800)
+menu2 = Menu.new(name: "すし", price: 1000)
+menu3 = Menu.new(name: "コーラ", price: 300)
+menu4 = Menu.new(name: "お茶", price: 200)
 
-puts menu1.info
+menus = [menu1, menu2, menu3, menu4]
+
+menus.each do |menu|
+  puts menu.info
+end
