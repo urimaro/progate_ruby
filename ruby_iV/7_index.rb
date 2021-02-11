e84fc8d3ae3f6bbbc24c75acdc89df75c62c81f5
Goh Matsumoto <urimaro@gmail.com>
2021-02-11 12:39:55 +0900

    15. Displaying the Menu (2) - 3. The Food Ordering App

--- ruby_iV/7_index.rb
+++ ruby_iV/7_index.rb
@@ -7,6 +7,9 @@ menu4 = Menu.new(name: "お茶", price: 200)
 
 menus = [menu1, menu2, menu3, menu4]
 
+index = 0
+
 menus.each do |menu|
-  puts menu.info
+  puts "#{index}. " + menu.info
+  index += 1
 end
