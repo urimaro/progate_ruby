5e73d92065081a4e9f5363aed071d7967b2d0a70
Goh Matsumoto <urimaro@gmail.com>
2021-02-11 15:09:39 +0900

    6. Food Ordering App Review - 1. Class Inheritance

--- ruby_v/index.rb
+++ ruby_v/index.rb
@@ -2,6 +2,26 @@ require "./food"
 require "./drink"
 
 food1 = Food.new(name: "ピザ", price: 800)
-food1.calorie = 700
+food2 = Food.new(name: "すし", price: 1000)
+drink1 = Drink.new(name: "コーラ", price: 300)
+drink2 = Drink.new(name: "お茶", price: 200)
 
-puts food1.calorie_info
+menus = [food1, food2, drink1, drink2]
+
+index = 0
+menus.each do |menu|
+  puts "#{index}. #{menu.info}"
+  index += 1
+end
+
+puts "--------------"
+puts "メニューの番号を選択してください"
+order = gets.chomp.to_i
+
+selected_menu = menus[order]
+puts "選択されたメニュー: #{selected_menu.name}"
+
+puts "個数を入力してください(3つ以上で100円割引)"
+count = gets.chomp.to_i
+
+puts "お会計は#{selected_menu.get_total_price(count)}円です"
