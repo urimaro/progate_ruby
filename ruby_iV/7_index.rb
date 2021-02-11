01bde535aeb5ce22816d2f24e849b6e20d893947
Goh Matsumoto <urimaro@gmail.com>
2021-02-11 12:56:23 +0900

    17. Selecting a Menu Item - 3. The Food Ordering App

--- ruby_iV/7_index.rb
+++ ruby_iV/7_index.rb
@@ -8,8 +8,18 @@ menu4 = Menu.new(name: "お茶", price: 200)
 menus = [menu1, menu2, menu3, menu4]
 
 index = 0
-
 menus.each do |menu|
   puts "#{index}. " + menu.info
   index += 1
 end
+
+puts "--------------"
+puts "メニューの番号を選択してください"
+
+order = gets.chomp.to_i
+selected_menu = menus[order]
+puts "選択されたメニュー: #{selected_menu.name}"
+
+puts "個数を入力してください(3つ以上で100円割引)"
+count = gets.chomp.to_i
+puts "お会計は#{selected_menu.get_total_price(count)}円です"
